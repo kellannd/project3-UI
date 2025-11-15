5ddc2bce1820b1a1ff0468eb225857bc897b49e0
kelly deal <kellyanndeal@gmail.com>
2025-11-14 22:49:20 -0500

    styling for events and orgs. mostly working event and location forms.

--- getinvolved-uc/src/lib/LocationForm.svelte
+++ getinvolved-uc/src/lib/LocationForm.svelte
@@ -2,6 +2,35 @@
   export let locationForm;
 
   export let onCampus;
+
+  export let location;
+
+  function testOnlineLocation() {
+    location.title = "Online Location";
+    location.address = "https://google.com";
+  }
+
+
+  // off campus
+  let offCampusTitle = ""
+  let offCampusAddress = ""
+  let offCampusCity = ""
+  let offCampusState = ""
+
+  // online
+  let onlineLink = ""
+
+  function offCampusLocation() {
+    location.title = offCampusTitle;
+    location.address = offCampusAddress;
+    location.city = offCampusCity + ", " + offCampusState
+
+  }
+
+  function onlineLocation() {
+    location.title = "Online Location"
+    location.address = onlineLink
+  }
 </script>
 
 <button
@@ -10,63 +39,113 @@
     locationForm = false;
   }}><i class="bi bi-x-lg"></i></button
 ><br />
-<div style=" display: flex; justify-content: center;">
-  <div>
 
+<div>
+<div class="flex-disp">
+  <div>
     <div class="location">
-      <div style="display: flex; justify-content: center">
-        <button onclick={() => {onCampus = "oncampus"
-        }} class="location-btn" autofocus>on campus</button>
-        <button onclick={() => {onCampus = "offcampus"
-        }} class="location-btn">off campus</button>
-        <button onclick={() => {onCampus = "online"}} class="location-btn">online</button>
+      <div class="flex-disp">
+        <button
+          onclick={() => {
+            onCampus = "oncampus";
+          }}
+          class="location-btn {onCampus === 'oncampus' ? 'selected' : ''}"
+          >On Campus</button
+        >
+        <button
+          onclick={() => {
+            onCampus = "offcampus";
+          }}
+          class="location-btn {onCampus === 'offcampus' ? 'selected' : ''}"
+          >Off Campus</button
+        >
+        <button
+          onclick={() => {
+            onCampus = "online";
+          }}
+          class="location-btn {onCampus === 'online' ? 'selected' : ''}"
+          >Online</button
+        >
       </div>
 
       {#if onCampus === "offcampus"}
-        <label for="loc-name"><span class="req">*</span>Location Name:</label><br>
-        <input type="text" id="loc-name" name="loc-name" style="width: 500px"/><br>
+      <div class="off-campus-inputs">
+        <div>
+        <label for="loc-name"><span class="req">*</span>Location Name:</label
+        ><br />
+        <input
+        bind:value={offCampusTitle}
+          type="text"
+          id="loc-name"
+          name="loc-name"
+          style="width: 500px"
+        /><br /><br /><br />
 
-        <label for="loc-address"><span class="req">*</span>Address:</label><br>
-        <input type="text" id="loc-address" name="loc-address" style="width: 500px"/><br>
+        <label for="loc-address"><span class="req">*</span>Address:</label><br
+        />
+        <input
+        bind:value={offCampusAddress}
+          type="text"
+          id="loc-address"
+          name="loc-address"
+          style="width: 500px"
+        /><br /><br /><br />
 
-        <label for="loc-city">City:</label><br>
-        <input type="text" id="loc-city" name="loc-city" style="width: 500px"/><br>
+        <label for="loc-city">City:</label><br />
+        <input
+        bind:value={offCampusCity}
+          type="text"
+          id="loc-city"
+          name="loc-city"
+          style="width: 500px"
+        /><br /><br /><br />
 
-        <label for="loc-state">State or Province:</label><br>
-        <input type="text" id="loc-staet" name="loc-state" style="width: 500px"/><br>
+        <label for="loc-state">State or Province:</label><br />
+        <input
+        bind:value={offCampusState}
+          type="text"
+          id="loc-staet"
+          name="loc-state"
+          style="width: 500px"
+        /><br /><br /><br />
 
-        {:else if onCampus === "online"}
-        <label for="online-link">Online Location Link:</label><br>
-        <input type="text" id="online-link" name="online-link" style="width: 500px"><br>
+        <div style="text-align: center">
+        <button class="add-loc-btn" onclick = {() => offCampusLocation() }>Add Location</button>
+        </div>
+        </div>
+        </div>
+      {:else if onCampus === "online"}
+      <div class="online-inputs">
+        <div>
+        <label for="online-link">Online Location Link:</label><br />
+        <input
+        bind:value={onlineLink}
+          type="text"
+          id="online-link"
+          name="online-link"
+          style="width: 500px"
+        /><br /><br /><br />
 
+        <label for="online-instructions"
+          >Online Location Instructions for Attendees:</label
+        ><br />
+        <textarea
+          id="online-instructions"
+          name="online-instructions"
+          style="width: 500px"
+        ></textarea><br><br /><br />
 
-        <label for="online-instructions">Online Location Instructions for Attendees:</label><br>
-        <textarea id="online-instructions" name="online-instructions" style="width: 500px"></textarea>
-
-
+        <div style="text-align: center">
+        <button class="add-loc-btn" onclick={() => onlineLocation() }>Add Location</button>
+        </div>
+        </div>
+        </div>
       {/if}
     </div>
   </div>
 </div>
+</div>
 
 <style>
   @import "../app.css";
-
-  .location {
-    border-style: solid;
-    border-width: 1px;
-    border-color: black;
-    width: 1000px;
-    height: 400px;
-    padding: 20px;
-  }
-
-  .location-btn {
-    padding: 10px;
-    margin: 10px;
-  }
-
-  .location-btn:focus {
-    background-color: red
-  }
 </style>
---
+++ getinvolved-uc/src/lib/Orgs.svelte
@@ -0,0 +1,35 @@
+<script>
+  let orgs = {
+    orgs: [
+      {
+        name: "University of Cincinnati ACM-W Student Chapter",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      },
+    ],
+  };
+
+  import acmwlogo from "../assets/acmw.jpg";
+</script>
+
+<div class="flex-disp">
+  {#each orgs.orgs as org}
+    <div class="org-info">
+      <div>
+        <img
+          src={acmwlogo}
+          style="width: 75px; height: 75px; border-radius: 50%; margin-right: 20px"
+        />
+      </div>
+
+      <div>
+        <h3 style="padding-bottom: 10px">{org.name}</h3>
+        <p>{org.description}</p>
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+@import "../app.css";
+</style>
